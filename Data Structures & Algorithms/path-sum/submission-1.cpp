241b5920e98af16c3f5e4283369444b2c9c2bc79
Nguyễn Đình Khánh Duy <duy07ntkh@gmail.com>
2026-09-03 17:11:12 +0700

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool dfs(TreeNode* root, int curSum, int target){
+        if (!root) return false;
+        curSum += root->val;
+        if (root->left == NULL && root->right == NULL){
+            return curSum == target;
+        }
+        return dfs(root->left, curSum, target) || dfs(root->right, curSum, target);
+    }
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return dfs(root, 0, targetSum);
+    }
+};

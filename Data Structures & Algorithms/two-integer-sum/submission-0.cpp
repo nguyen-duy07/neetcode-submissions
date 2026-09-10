d8c78526b3035f178893668eb7ff9e8583a3f6c3
Nguyễn Đình Khánh Duy <duy07ntkh@gmail.com>
2026-09-10 15:57:23 +0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++){
+            int com = target - nums[i];
+            if (seen.count(com)) {
+                return {seen[com], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};

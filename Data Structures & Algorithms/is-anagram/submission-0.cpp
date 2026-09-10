599223b3ce7b24523475eff77141eaa8e06292ed
Nguyễn Đình Khánh Duy <duy07ntkh@gmail.com>
2026-09-10 16:15:07 +0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int arr[26] = {0};
+        for (char c : s){
+            arr[c - 'a']++;
+        }
+        for (char c : t){
+            arr[c - 'a']--;
+            if (arr[c - 'a'] < 0) return false;
+        }
+        return true;
+    }
+};

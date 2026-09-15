a742aa63543ce7689640eed7424b054e31243c54
Nguyễn Đình Khánh Duy <duy07ntkh@gmail.com>
2026-09-15 15:21:53 +0700

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private: 
+    int m, n;
+    void dfs(vector<vector<int>>& image, int r, int c, int orig, int color){
+        if (r < 0 || c < 0 || r >= m || c >= n || image[r][c] != orig) return;
+        image[r][c] = color;
+        dfs(image, r + 1, c, orig, color);
+        dfs(image, r - 1, c, orig, color);
+        dfs(image, r, c + 1, orig, color);
+        dfs(image, r, c - 1, orig, color);
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int orig = image[sr][sc];
+        if (orig == color) return image;
+        m = image.size();
+        n = image[0].size();
+        dfs(image, sr, sc, orig, color);
+        return image;
+    }
+};
